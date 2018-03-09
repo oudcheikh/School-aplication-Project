f235c1c3fb4f44149542606df79af74c13cdd0bb
oudcheikh <33845168+oudcheikh@users.noreply.github.com>
2018-03-09 14:13:50 +0100

    Update connectToCard.py

--- ACR122Driver/connectToCard.py
+++ ACR122Driver/connectToCard.py
@@ -9,27 +9,29 @@ getuid = [0xFF, 0xCA, 0x00, 0x00, 0x00]
 #trouver le lecteur
 reader = getreader()[0]
 
-#Connecter au lecteur
-connection = reader.createConnection()
-
-#inctancie  la classe pour vhervher pour n'import quel type de carte
-cardtype = AnyCardType()
-
-#attendre qu'une carte aparaisse
-cardrequest = CardRequest(timeout=10*60, cardType=cardtype)
-
-try:
-    #on attend pour 10*60 seconde
-    cardservice = cardrequest.waitforcard()
+if  reader == False:
+    'Le lecteur n est pas trouver'
+    break
+else:
+    #Connecter au lecteur
+    connection = reader.createConnection()
     
-    #on se connecte avecla carte apparu via le lecteur
-    connection.connect()
-    #on envoie la commande qui lu ID de la carte 
-    data, sw1, sw2 = connection.transmit(getuid)
-    
-    print (data, sw1, sw2)
+    #inctancie  la classe pour vhervher pour n'import quel type de carte
+    cardtype = AnyCardType()
+    #attendre qu'une carte aparaisse
+    cardrequest = CardRequest(timeout=10*60, cardType=cardtype)
+    try:
+        #on attend pour 10*60 seconde
+        cardservice = cardrequest.waitforcard()
 
-except Exception as e:
-    if str(e) == 'Time-out during card request':
-        print (e)
+        #on se connecte avecla carte apparu via le lecteur
+        connection.connect()
+        #on envoie la commande qui lu ID de la carte 
+        data, sw1, sw2 = connection.transmit(getuid)
+
+        print (data, sw1, sw2)
+
+    except Exception as e:
+        if str(e) == 'Time-out during card request':
+            print (e)
 
